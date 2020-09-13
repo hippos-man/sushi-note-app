53431e706c12c42c647db11c16d4231018353b94
mana-tea <ma571na.tee@gmail.com>
2020-09-13 15:34:48 +0800

    Prepared some user & Todo test datas

---
+++ docker/mysql/initdb.d/testdata.sql
@@ -0,0 +1,47 @@
+INSERT INTO users
+    (
+    user_id,
+    first_name,
+    last_name,
+    email_address,
+    password,
+    is_active,
+    created_date_time,
+    updated_date_time
+    )
+VALUES
+    (
+    "john777",
+    "John",
+    "Smith",
+    "hoge@gmail.com",
+    "password",
+    true,
+    "2020-10-10-01:01:01",
+    NULL
+    );
+
+
+INSERT INTO todos
+(
+    todo_id,
+    title,
+    description,
+    deadline_date,
+    is_completed,
+    created_date_time,
+    updated_date_time,
+    label_id,
+    user_id
+)
+VALUES
+(
+    "fdjaofjaojfd",
+    "Go to the beach",
+    "A day off!! Wake up in the morning, and borrow the car from my friend. Let's go beach",
+    NULL,
+    false,
+    "2020-08-24 10:00:00",
+    NULL,
+    "john777"
+);
