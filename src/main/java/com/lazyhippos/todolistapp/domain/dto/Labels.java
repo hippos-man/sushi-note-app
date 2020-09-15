6431f0f7ca2f4aefc0e443f43c33ca18b91ddda8
mana-tea <ma571na.tee@gmail.com>
2020-09-15 20:58:59 +0800

    Created users & todos & labels Jpa entity class

---
+++ src/main/java/com/lazyhippos/todolistapp/domain/dto/Labels.java
@@ -0,0 +1,74 @@
+package com.lazyhippos.todolistapp.domain.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Labels {
+
+    @Id
+    private String labelId;
+    private String labelName;
+    private String description;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime updatedDateTime;
+    private String userId;
+    private Boolean isDeleted;
+
+    public String getLabelId() {
+        return labelId;
+    }
+
+    public void setLabelId(String labelId) {
+        this.labelId = labelId;
+    }
+
+    public String getLabelName() {
+        return labelName;
+    }
+
+    public void setLabelName(String labelName) {
+        this.labelName = labelName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public LocalDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public void setUpdatedDateTime(LocalDateTime updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
