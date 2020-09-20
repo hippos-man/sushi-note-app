5300dedda94f39accd3bc0351ba9d9e0db962fec
hippos-man <hipposwave@gmail.com>
2020-09-20 20:00:26 +0800

    feat:Register new todo

---
+++ src/main/java/com/lazyhippos/todolistapp/application/TodoForm.java
@@ -0,0 +1,22 @@
+package com.lazyhippos.todolistapp.application;
+
+import java.io.Serializable;
+
+public class TodoForm implements Serializable {
+
+    private String title;
+
+    public TodoForm(){}
+
+    public TodoForm(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
