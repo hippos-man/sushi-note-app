22737eee0acec560cf2ae8abf739a7f88ba7ce8c
hippos-man <hipposwave@gmail.com>
2020-09-27 11:28:50 +0800

    [WIP] Replace mock input in User controller

--- src/main/java/com/lazyhippos/todolistapp/application/resource/UserRequest.java
+++ src/main/java/com/lazyhippos/todolistapp/application/resource/UserRequest.java
@@ -1,4 +1,62 @@
 package com.lazyhippos.todolistapp.application.resource;
 
 public class UserRequest {
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String password;
+
+    public UserRequest() {
+    }
+
+    public UserRequest(String userId, String firstName, String lastName, String emailAddress,
+                       String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
