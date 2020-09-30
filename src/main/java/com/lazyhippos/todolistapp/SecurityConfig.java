1a62a9f67dc772ac387af17ed704c3893bffd7ef
hippos-man <hipposwave@gmail.com>
2020-09-30 22:20:15 +0800

    [WIP] Need to create Post method for login

--- src/main/java/com/lazyhippos/todolistapp/SecurityConfig.java
+++ src/main/java/com/lazyhippos/todolistapp/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.lazyhippos.todolistapp;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -12,12 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeRequests()
+                .antMatchers("/public/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
-                .and()
-                .httpBasic();
+                .loginPage("/login")
+                .failureUrl("/login")
+                .permitAll();
     }
 
     @Override
