d9b0fce90306e51af46e9f4abf6bc649a48fdf11
hippos-man <hipposwave@gmail.com>
2020-10-04 13:04:40 +0800

    Changed endpoint for user controller to simplify structure

--- src/main/java/com/lazyhippos/todolistapp/application/controller/UserController.java
+++ src/main/java/com/lazyhippos/todolistapp/application/controller/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
 
+@RequestMapping("/user")
 @Controller
 public class UserController {
 
@@ -19,14 +21,14 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/register")
+    @GetMapping("/sign-up")
     public String showUserRegisterPage(Model model){
         model.addAttribute("request", new UserRequest());
         return "register";
     }
 
 
-    @PostMapping("/user/register")
+    @PostMapping("/register")
     public String register(@ModelAttribute UserRequest request){
         // Get current time
         LocalDateTime now = LocalDateTime.now();
