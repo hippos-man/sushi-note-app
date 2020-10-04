d7f9fc382da7ad43bd6b183db271061893d9785c
hippos-man <hipposwave@gmail.com>
2020-10-04 20:37:17 +0800

    [WIP] Filtered completed to-do items from list. Will add switching of completion feature

--- src/main/java/com/lazyhippos/todolistapp/SecurityConfig.java
+++ src/main/java/com/lazyhippos/todolistapp/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("/to-do/all", true)
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login")
