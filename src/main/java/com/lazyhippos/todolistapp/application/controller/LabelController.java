ca97ee9b6644dd871c240070bdc06d938bf898f8
hippos-man <hipposwave@gmail.com>
2020-10-07 20:22:43 +0800

    Added mock Label area in Main page

--- src/main/java/com/lazyhippos/todolistapp/application/controller/LabelController.java
+++ src/main/java/com/lazyhippos/todolistapp/application/controller/LabelController.java
@@ -1,4 +1,12 @@
 package com.lazyhippos.todolistapp.application.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LabelController {
+
+    // TODO Label Register page
+    public String showLabelDetail(){
+        return "labelDetail";
+    }
 }
