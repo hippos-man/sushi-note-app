b8400c30de38b20d94cbed91488830e8d4ecd9af
hippos-man <hipposwave@gmail.com>
2020-10-19 23:16:42 +0800

    Added Sign up form Validation

--- src/main/java/com/lazyhippos/todolistapp/application/controller/UserController.java
+++ src/main/java/com/lazyhippos/todolistapp/application/controller/UserController.java
@@ -5,12 +5,12 @@ import com.lazyhippos.todolistapp.domain.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 @RequestMapping("/user")
@@ -31,7 +31,7 @@ public class UserController {
 
 
     @PostMapping("/register")
-    public String register(@Validated @ModelAttribute UserRequest request, BindingResult bindingResult, Model model){
+    public String register(@Valid @ModelAttribute(name = "request") UserRequest request, BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()){
             System.out.println("Binding Result Error: " + bindingResult.getAllErrors());
             model.addAttribute("request", request);
