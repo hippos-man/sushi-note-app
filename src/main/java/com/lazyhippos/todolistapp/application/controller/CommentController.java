d113b30a38f6bb1736b107b8dbe82bffd5a318b1
hippos-man <hipposwave@gmail.com>
2020-12-16 08:36:58 +0800

    Added Comment read feature close #77

---
+++ src/main/java/com/lazyhippos/todolistapp/application/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.lazyhippos.todolistapp.application.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommentController {
+
+    // TODO POST method
+
+    // TODO UPDATE method
+}
