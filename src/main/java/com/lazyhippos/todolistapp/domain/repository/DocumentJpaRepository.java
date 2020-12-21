bdcb5769cca81eff8515d98211075e6406071574
hippos-man <hipposwave@gmail.com>
2020-12-21 23:14:35 +0800

    Tested upload method. Will implement with Article registration.

--- src/main/java/com/lazyhippos/todolistapp/domain/repository/DocumentJpaRepository.java
+++ src/main/java/com/lazyhippos/todolistapp/domain/repository/DocumentJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageJpaRepository extends JpaRepository<Documents, Long> {
+public interface DocumentJpaRepository extends JpaRepository<Documents, Long> {
 }
--- src/main/java/com/lazyhippos/todolistapp/domain/service/DocumentService.java
+++ src/main/java/com/lazyhippos/todolistapp/domain/service/DocumentService.java
@@ -1,7 +1,19 @@
 package com.lazyhippos.todolistapp.domain.service;
 
+import com.lazyhippos.todolistapp.domain.model.Documents;
+import com.lazyhippos.todolistapp.domain.repository.DocumentJpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DocumentService {
+
+    public final DocumentJpaRepository documentJpaRepository;
+
+    DocumentService (DocumentJpaRepository documentJpaRepository) {
+        this.documentJpaRepository = documentJpaRepository;
+    }
+
+    public void save(Documents documents) {
+        documentJpaRepository.save(documents);
+    }
 }
