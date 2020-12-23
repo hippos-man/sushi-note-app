69c2466c73b71e011e819908517943ab97202ef2
hippos-man <hipposwave@gmail.com>
2020-12-23 22:29:21 +0800

    Implemented article creation method with image uploading feature. Will show image in article page.

--- src/main/java/com/lazyhippos/todolistapp/domain/repository/DocumentJpaRepository.java
+++ src/main/java/com/lazyhippos/todolistapp/domain/repository/DocumentJpaRepository.java
@@ -3,6 +3,7 @@ package com.lazyhippos.todolistapp.domain.repository;
 import com.lazyhippos.todolistapp.domain.model.Documents;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ import java.util.List;
 public interface DocumentJpaRepository extends JpaRepository<Documents, Long> {
     @Query("SELECT new Documents(d.documentId, d.originalName, d.fileSize) FROM Documents d ORDER BY d.createdDateTime DESC")
     List<Documents> findAll();
+
+    @Query("SELECT d.documentId FROM Documents d WHERE d.originalName = :fileName")
+    Long getDocumentIdByOriginalName(@Param(value = "fileName") String fileName);
 }
