555da18601574382407a02f76d62b12516c21fc3
hippos-man <hipposwave@gmail.com>
2020-12-28 12:56:17 +0800

    Inserted test data (likes)

---
+++ src/main/java/com/lazyhippos/todolistapp/domain/model/Likes.java
@@ -0,0 +1,49 @@
+package com.lazyhippos.todolistapp.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.time.LocalDateTime;
+
+@Entity
+@IdClass(LikesId.class)
+public class Likes {
+    @Id
+    private String userId;
+    @Id
+    private String articleId;
+    private LocalDateTime createdDateTime;
+
+    public Likes() {
+    }
+
+    public Likes(String userId, String articleId, LocalDateTime createdDateTime) {
+        this.userId = userId;
+        this.articleId = articleId;
+        this.createdDateTime = createdDateTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId;
+    }
+
+    public LocalDateTime getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+}
